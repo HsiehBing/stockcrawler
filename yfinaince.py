e9cd782ebeb17b3228d02a424e7f427736a28f54
david2881234 <david2881234@gmail.com>
2022-08-27 16:00:20 +0000

    Replace currentPrice with regularMarketPrice

--- yfinaince.py
+++ yfinaince.py
@@ -137,19 +137,19 @@ def finainces(msg):
         up_down=[]
         Change_Rate = 0
         Ticker = yf.Ticker(StockName)
-        Tiker2 = Ticker.info['currentPrice']
+        Tiker2 = Ticker.info['regularMarketPrice']
 
         Previous_Price = str(Ticker.info['previousClose'])
-        Current_Price0 = str(Ticker.info['currentPrice'])
+        Current_Price0 = str(Ticker.info['regularMarketPrice'])
         Current_Price = Current_Price0[:5]
-        Price_Gap00 = Ticker.info['currentPrice']-Ticker.info['previousClose']
+        Price_Gap00 = Ticker.info['regularMarketPrice']-Ticker.info['previousClose']
         Price_Gap0 = str(Price_Gap00)
         Price_Gap = Price_Gap0[:5]
         if Price_Gap00 >0:
             up_down = '漲'
         elif Price_Gap00 <0:
             up_down = '跌'
-        Change_Rate00 = (Ticker.info['currentPrice']-Ticker.info['previousClose'])/ Ticker.info['previousClose']*100
+        Change_Rate00 = (Ticker.info['regularMarketPrice']-Ticker.info['previousClose'])/ Ticker.info['previousClose']*100
         Change_Rate0 = str(Change_Rate00)
         if Change_Rate00 >=0:
             Change_Rate = Change_Rate0[:3]
