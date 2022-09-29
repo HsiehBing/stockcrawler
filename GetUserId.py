bec4364934598edc78711e1d8a24bc3d01c41ec7
HsiehBing <godadison@gmail.com>
2022-09-30 00:35:32 +0800

    getuseridT3

--- GetUserId.py
+++ GetUserId.py
@@ -1,6 +1,6 @@
 from linebot import LineBotApi
 
 def GETUserId(event):
-    UserId = str(event.source.user_id)
-    # profile = line_bot_api.get_profile(UserId)
-    return UserId
+    UserId = event.source.user_id
+    profile = line_bot_api.get_profile(UserId)
+    return profile
